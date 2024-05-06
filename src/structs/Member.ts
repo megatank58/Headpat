e90988be4f626bdcfb8b7d63907965070cbc7de5
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2024-05-07 00:45:42 +0300

    fix<back>: user/member typings

--- src/structs/Member.ts
+++ src/structs/Member.ts
@@ -1,8 +1,6 @@
 export default class Member {
     ID: string;
     username: string;
-    avatar: string;
-    banner: string;
     discriminator: string;
     role: string;
     createdAt: string;
