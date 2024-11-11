047fd56146e33ff0e11bfff2b201c778a23a4793
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2024-11-11 16:25:47 +0200

    chore<rtc>: enforce TURN relay

--- src/html/scripts/app.js
+++ src/html/scripts/app.js
@@ -669,7 +669,8 @@ async function createNewPeer(id){
                 username: iceCredentials.username,
                 credential: iceCredentials.credential
             },
-        ]
+        ],
+        iceTransportPolicy: "relay"
     });
     localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
 
